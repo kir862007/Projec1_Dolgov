cbd0b0998afc70e28c28badf4400b390ebdca6f5
Enotaaa <kir862001@gmail.com>
2025-09-13 10:08:17 +0300

    zadanie1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.StringBuilder
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>)
+{
+ val input = "AAADSSSRRTTHAAAA"
+    val output = vhodstroki(input)
+    println(output)
 }
+
+fun vhodstroki(input: String):String {
+    if (input.isEmpty()) return ""
+    val result = StringBuilder()
+    var count = 1
+
+    for (i in 1 until input.length) {
+        if (input[i] == input[i - 1]) {
+            count++
+        } else {
+            result.append(input[i-1])
+            if (count > 1) {
+                result.append(count)
+            }
+            count = 1
+        }
+    }
+    result.append(input[input.length - 1])
+    if(count>1)
+    {
+        result.append(count)
+    }
+    return result.toString()
+}
+
